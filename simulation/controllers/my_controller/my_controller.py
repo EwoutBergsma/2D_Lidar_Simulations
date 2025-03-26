4c750655667d6455dfb2cc6ae8c0fa1b9c77b5ec
EwoutBergsma <43827037+EwoutBergsma@users.noreply.github.com>
2025-03-26 16:29:57 +0100

    updated the simulation to change dynamically the arrangement of sensors

--- simulation/controllers/my_controller/my_controller.py
+++ simulation/controllers/my_controller/my_controller.py
@@ -2,10 +2,12 @@
 
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+# from controller import Robot  # old
+from controller import Supervisor
 
 # create the Robot instance.
-robot = Robot()
+# robot = Robot()  # old
+robot = Supervisor()
 
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
@@ -22,19 +24,26 @@ right_motor.setVelocity(3.8)
 
 left_lidar = robot.getDevice('LDS-01')
 right_lidar = robot.getDevice('LDS-02')
+
 left_lidar.enable(timestep)
 right_lidar.enable(timestep)
 
 left_lidar.enablePointCloud()
 right_lidar.enablePointCloud()
 
-ewout = []
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
+lidar_node = robot.getFromDef('lds_01')  # It cant find lds01def
+lidar_node_translation_field = lidar_node.getField('translation')
+lidar_node_translation_field.setSFVec3f([0,0,0])
+
+lidar_node = robot.getFromDef('lds_02')  # It cant find lds01def
+lidar_node_translation_field = lidar_node.getField('translation')
+lidar_node_translation_field.setSFVec3f([0,0,0])
+
+# ewout = []
 while robot.step(timestep) != -1:
     range_image = left_lidar.getRangeImage()
-    ewout.append(range_image)
+    # ewout.append(range_image)
     range_image = right_lidar.getRangeImage()
-    ewout.append(range_image)
+    # ewout.append(range_image)
     pass
 
